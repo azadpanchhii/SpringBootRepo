38a53a2cb527180c986d243ff36d6c65120b8c94
Deepak Sharma <deepak.sharma651@gmail.com>
2019-06-13 15:43:47 +0530

    Added eureka server

---
+++ eureka-server/src/main/java/com/deepak/server/DiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.deepak.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableZuulProxy
+public class DiscoveryServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+}
